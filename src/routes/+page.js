9283bdac90587310b23d8b9680382125ce4edd3a
badwolf11 <mbennett1190@gmail.com>
2022-09-29 21:44:31 +1000

    trying...

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,6 +1,7 @@
 export async function load ({params, url}) {
     let actor = url.searchParams.get('actor');
     let name = JSON.parse(actor);
+    //let name = actorDets.name;
     return {
         name
     };
