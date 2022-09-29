3f2c546f6eb7f4006c18ddd3b75445aedac28830
badwolf11 <mbennett1190@gmail.com>
2022-09-29 21:47:07 +1000

    done!

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,9 +1,9 @@
 export async function load ({params, url}) {
     let actor = url.searchParams.get('actor');
-    let name = JSON.parse(actor);
+    let actorDets = JSON.parse(actor);
     //let name = actorDets.name;
     return {
-        name
+        actorDets
     };
    
 };
