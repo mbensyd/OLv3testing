420e1ec85b484ed5f96a2d955b4b99cfc8f1529a
badwolf11 <mbennett1190@gmail.com>
2022-09-30 16:23:31 +1000

    new url search

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,6 +1,8 @@
 export async function load ({params, url}) {
     let actor = url.searchParams.get('actor');
     let actorDets = JSON.parse(actor);
+    let urlDets = url.searchParams.get('');
+    console.log(urlDets);
     //let name = actorDets.name;
     return {
         actorDets
