87c694f51224f60a8a9f84d97ed72df586fa970c
badwolf11 <mbennett1190@gmail.com>
2022-09-30 16:43:15 +1000

    urltest

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,20 +1,12 @@
 export async function load ({params, url}) {
-    let actor = url.searchParams.get('actor');
-    let actorDets = JSON.parse(actor);
+    let auth = url.searchParams.get('auth');
+   // let actorDets = JSON.parse(actor);
 
-    var endpoint = url.searchParams.get('endpoint');
-    var auth = url.searchParams.get('auth');
-    var activity_id = urlParams.get('activity_id');
-    var registration = urlParams.get('registration');
+    
 
     //let name = actorDets.name;
     return {
-        "actorDets" : actorDets,
-        "endPoint": endpoint,
-        "authDets": auth,
-        "actID": activity_id,
-        "reg": registration,
+        auth
     };
-    
-    
+   
 };
