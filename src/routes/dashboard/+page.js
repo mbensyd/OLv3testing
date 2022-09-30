8b744c652ccc4d93ad0441b99fce7957d48fc058
badwolf11 <mbennett1190@gmail.com>
2022-09-30 10:49:35 +1000

    test

--- src/routes/dashboard/+page.js
+++ src/routes/dashboard/+page.js
@@ -1,3 +1,10 @@
+import mod1 from '$lib/images/mod1.png';
+import mod2 from '$lib/images/mod2.png';
+import { modules } from "$lib/data/module.json";
+
+
 export async function load (page) {
-    
+    if (modules[0].image) {
+        return mod1
+    } 
 };
--- src/routes/dashboard/[mod]/+page.js
+++ src/routes/dashboard/[mod]/+page.js
@@ -1,6 +1,7 @@
 import { modules } from "$lib/data/module.json";
 import { page } from '$app/stores';
 
+
 export async function get(page) {
 
 }
