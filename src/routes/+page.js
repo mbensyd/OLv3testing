4176cd71ab366f514f477be2c99b75dd353f88d1
badwolf11 <mbennett1190@gmail.com>
2022-09-30 16:37:11 +1000

    details from xapi

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,11 +1,20 @@
 export async function load ({params, url}) {
     let actor = url.searchParams.get('actor');
     let actorDets = JSON.parse(actor);
-    let urlDets = url.searchParams.get('');
-    console.log(urlDets);
+
+    var endpoint = url.searchParams.get('endpoint');
+    var auth = url.searchParams.get('auth');
+    var activity_id = urlParams.get('activity_id');
+    var registration = urlParams.get('registration');
+
     //let name = actorDets.name;
     return {
-        actorDets
+        "actorDets" : actorDets,
+        "endPoint": endpoint,
+        "authDets": auth,
+        "actID": activity_id,
+        "reg": registration,
     };
-   
+    
+    
 };
