9f53a842d18834b31d8d6eba3145a01e688eae73
badwolf11 <mbennett1190@gmail.com>
2022-10-04 10:59:32 +1100

    fix?

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -2,11 +2,13 @@ export async function load ({params, url}) {
     let actor = url.searchParams.get('actor');
     let reg = url.searchParams.get('registration');
     let actorDets = JSON.parse(actor);
+    var urlAdd = window.location.search;
 
-    //let name = actorDets.name;
     return {
         actorDets,
         reg,
-    };
+    }; 
+
+    
    
 };
