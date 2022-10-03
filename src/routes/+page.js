d7f9b034c4683512a14d6deea864b344831b1a1c
badwolf11 <mbennett1190@gmail.com>
2022-10-04 10:50:54 +1100

    fix attempt 1: get url dets for button

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,14 +1,12 @@
 export async function load ({params, url}) {
     let actor = url.searchParams.get('actor');
     let reg = url.searchParams.get('registration');
-    var urlAdd = window.location.search;
     let actorDets = JSON.parse(actor);
 
     //let name = actorDets.name;
     return {
         actorDets,
         reg,
-        urlAdd
     };
    
 };
