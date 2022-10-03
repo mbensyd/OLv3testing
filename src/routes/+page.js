79d7bbc8c67cc6617ee030d7adae66cb2be3d563
badwolf11 <mbennett1190@gmail.com>
2022-10-04 10:47:38 +1100

    get URL and add to button

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,14 +1,14 @@
 export async function load ({params, url}) {
     let actor = url.searchParams.get('actor');
     let reg = url.searchParams.get('registration');
-
+    var urlAdd = window.location.search;
     let actorDets = JSON.parse(actor);
-    
 
     //let name = actorDets.name;
     return {
         actorDets,
-        reg
+        reg,
+        urlAdd
     };
    
 };
