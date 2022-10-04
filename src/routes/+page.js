e5ed03af09efa63c8ea4aa48c4b609de72cf127d
badwolf11 <mbennett1190@gmail.com>
2022-10-04 11:05:32 +1100

    getting url first page.

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -2,13 +2,13 @@ export async function load ({params, url}) {
     let actor = url.searchParams.get('actor');
     let reg = url.searchParams.get('registration');
     let actorDets = JSON.parse(actor);
-    var urlAdd = window.location.search;
+
 
     return {
         actorDets,
         reg,
     }; 
 
-    
+
    
 };
