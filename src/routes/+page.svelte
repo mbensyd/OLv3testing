7b81403ce46177902428360cc080861a6bcb40ed
emilycollins4 <emtcollins4@gmail.com>
2022-10-04 12:05:20 +1100

    added underscore

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -47,7 +47,7 @@
     <div class="mt-32 flex justify-center items-center">
         <div class="p-16 max-w-2xl bg-white bg-opacity-70 rounded-3xl shadow-2xl dark:bg-gray-800 dark:border-gray-700">
                 <h5 class="mb-8 text-3xl font-bold tracking-tight text-gray-900 dark:text-white">Hey, <span class="font-normal">{actor.name}</span></h5>
-            <p class="mb-8 font-normal text-xl text-gray-800 dark:text-gray-700"><span class="font-bold">Your mission </span>is to conduct a life cycle assessment of the entire environmental management system (EMS) of your company with the aim of reaching net zero carbon emission reduction goals.</p>
+            <p class="mb-8 font-normal text-xl text-gray-800 dark:text-gray-700"><span class="font-bold">Your mission </span>i _ s to conduct a life cycle assessment of the entire environmental management system (EMS) of your company with the aim of reaching net zero carbon emission reduction goals.</p>
             <a data-sveltekit-prefetch href="/dashboard" class="mt-2 inline-flex py-2 px-7 text-l font-medium text-center rounded-full shadow-xl text-white bg-sky-500 border hover:text-sky-500 hover:font-bold hover:border border-sky-500 hover:bg-white hover:bg-opacity-60" >
                 LAUNCH LEARN SPACE
             </a>
