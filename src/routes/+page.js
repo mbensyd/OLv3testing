84af6dbb832e41cd4730d98ca9f8ef6b75a852ec
badwolf11 <mbennett1190@gmail.com>
2022-10-04 14:54:24 +1100

    test store

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,23 +1,10 @@
 import { dets } from './store.js';
+import { onMount } from 'svelte';
 
-
-export async function load ({params, url}) {
-    let actor = url.searchParams.get('actor');
-    let reg = url.searchParams.get('registration');
-    let endpoint = url.searchParams.get('endpoint');
-    let auth = url.searchParams.get('auth');
-
-    let actorName = JSON.parse(actor.name);
-    let actorMbox = JSON.parse(actor.mbox);
-
-
-    dets.set({
-        actorName,
-        actorMbox,
-        reg,
-        auth,
-        endpoint
-    })
+export async function load () {
+    let url = ``;
+    onMount(() => url = window.location.href);
+    dets.set(url);
 
 };
 
