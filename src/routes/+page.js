a1cbe7000743f44f2ce7cdafb8e539dd07de573b
badwolf11 <mbennett1190@gmail.com>
2022-10-04 15:49:15 +1100

    try

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,9 +1,8 @@
 import { dets } from './store.js';
-import { onMount } from 'svelte';
 
-export async function load () {
-    let url = ``;
-    onMount(() => url = window.location.href);
+export async function load ({params, url}) {
+    let uri = url.searchParams;
+    url = window.location.href;
     dets.set(url);
 
 };
