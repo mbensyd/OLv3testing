0d6823eb35374dfa9c0a62d5f881b462a379a704
badwolf11 <mbennett1190@gmail.com>
2022-10-04 11:16:05 +1100

    dets url auth

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,12 +1,16 @@
 export async function load ({params, url}) {
     let actor = url.searchParams.get('actor');
     let reg = url.searchParams.get('registration');
-    let actorDets = JSON.parse(actor);
+    let endpoint = url.searchParams.get('endpoint');
+    let auth = url.searchParams.get('auth');
 
+    let actorDets = JSON.parse(actor);
 
     return {
         actorDets,
         reg,
+        auth,
+        endpoint
     }; 
 
 
