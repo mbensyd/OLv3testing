efc8f82bfcc59a2dc7e58614b45930ef55c87997
badwolf11 <mbennett1190@gmail.com>
2022-10-04 14:31:41 +1100

    creating a store

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,18 +1,23 @@
+import { dets } from '..lib/Stores/store.js';
+
+
 export async function load ({params, url}) {
     let actor = url.searchParams.get('actor');
     let reg = url.searchParams.get('registration');
     let endpoint = url.searchParams.get('endpoint');
     let auth = url.searchParams.get('auth');
 
-    let actorDets = JSON.parse(actor);
+    let actorName = JSON.parse(actor.name);
+    let actorMbox = JSON.parse(actor.mbox);
 
-    return {
-        actorDets,
+
+    dets.set({
+        actorName,
+        actorMbox,
         reg,
         auth,
         endpoint
-    }; 
+    })
 
-
-   
 };
+
