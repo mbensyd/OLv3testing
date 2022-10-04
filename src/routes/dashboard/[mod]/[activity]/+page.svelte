9868941a57513dbb6b6c1a8577ab04d56c6f5263
badwolf11 <mbennett1190@gmail.com>
2022-10-04 21:36:48 +1100

    url fixed

--- src/routes/dashboard/[mod]/[activity]/+page.svelte
+++ src/routes/dashboard/[mod]/[activity]/+page.svelte
@@ -13,7 +13,7 @@
 
     console.log(regURL);
 
-    let urlRise = `https://media.openlearning.com/NLdnqLay7fgVSFY8j8dbuTaVy5SntPfn9dHEqqTYfQAvyS99BTkdTH6d4WcjKsFY.1664518486/demo-xapi-44M0T6Hc/index.html?activity_id=https%3A%2F%2Fwww.openlearning.com%2Fcourses%2Fanother-experiment%2Ftemp%2F%3Fblock%3D6336895226a58ba166fa72d9%26embedded%3Dtrue&actor=%7B%22objectType%22%3A%22Agent%22%2C%22name%22%3A%${nameURL}%22%2C%22mbox%22%3A%22${mboxURL}%22%7D&auth=${authURL}&endpoint=${endpointURL}&registration=${regURL}`;
+    let urlRise = `https://media.openlearning.com/NLdnqLay7fgVSFY8j8dbuTaVy5SntPfn9dHEqqTYfQAvyS99BTkdTH6d4WcjKsFY.1664518486/demo-xapi-44M0T6Hc/index.html?activity_id=https%3A%2F%2Fwww.openlearning.com%2Fcourses%2Fanother-experiment%2Ftemp%2F%3Fblock%3D6336895226a58ba166fa72d9%26embedded%3Dtrue&actor=%7B%22objectType%22%3A%22Agent%22%2C%22name%22%3A%22${nameURL}%22%2C%22mbox%22%3A%22${mboxURL}%22%7D&auth=${authURL}&endpoint=${endpointURL}&registration=${regURL}`;
 
     console.log(urlRise);
 </script>
