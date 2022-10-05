addf6edb9e2026b898bde977ef3dcb5242496509
badwolf11 <mbennett1190@gmail.com>
2022-10-05 12:06:54 +1100

    fixed button
    Co-authored-by: Emily Collins <E.Collins3@westernsydney.edu.au>

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -3,7 +3,6 @@
     import { dets } from '../store.js';
     import { page } from '$app/stores';
     import { modules } from "$lib/data/module.json";
-    import { modData } from '../store.js';
 
 
     //get xapi ?search from url mega hack don't do
@@ -12,7 +11,6 @@
 
     //testing data store from first screen
     console.log($dets);
-    console.log($modData);
 
     //images - need to be refactored so are dynamic. No idea how to link in sveltekit. 
     import mod1 from '$lib/images/mod1.png';
@@ -67,12 +65,12 @@
 
 <div class="flex flex-row">
     {#each modules as mod}
-    <div class="{clicked === `${mod.Title}` ? '' : 'closed'} basis-0 grow h-screen bg-cover hover:grow-[1.5] overlay {show === `${mod.Title}` ? 'active' : ''}  {current === `${mod.Title}` ? 'selected' : ''}" 
+    <div class="{clicked === `${mod.Title}` ? '' : 'closed'} basis-0 grow h-screen bg-cover hover:grow-[1.5] {show === `${mod.Title}` ? 'active' : ''}  {current === `${mod.Title}` ? 'selected' : ''}" 
     on:mouseenter="{() => current = `${mod.Title}`}" style="background-image: linear-gradient(1deg, rgb(0 0 0 / 0%), rgb(0 0 0 / 60%)), url('{mod.image}')">
         <div class="object-center mx-[10%] mt-28 flex flex-col justify-center items-center">
             <h1 class="text-3xl font-semibold pt-12 text-white">{mod.Title}</h1>
             <p class="pt-8 text-white">{mod.Description}</p>
-            <a class="mt-8 inline-flex py-2 px-7 text-l font-medium text-center rounded-full shadow-xl text-white bg-sky-500 border hover:text-sky-500 hover:font-bold hover:border border-sky-500 hover:bg-white hover:bg-opacity-60" 
+            <a href="/dashboard/${mod.Title}${$dets.newUrl}" class="mt-8 inline-flex py-2 px-7 text-l font-medium text-center rounded-full shadow-xl text-white bg-sky-500 border hover:text-sky-500 hover:font-bold hover:border border-sky-500 hover:bg-white hover:bg-opacity-60" 
             on:click="{() => show = `${mod.Title}`}" on:click="{() => clicked = `${mod.Title}`}" on:click="{press}"  on:click="{modPage}">
             LET'S GO </a>
 
