a2b84e44526d0cf4976f5ac329ecfc7b3f8ed887
emilycollins4 <emtcollins4@gmail.com>
2022-10-05 11:14:54 +1100

    change

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -65,13 +65,13 @@
 <div class="flex flex-row">
     {#each modules as mod}
     <div class="{clicked === `${mod.Title}` ? '' : 'closed'} basis-0 grow h-screen bg-cover hover:grow-[1.5] overlay {show === `${mod.Title}` ? 'active' : ''}  {current === `${mod.Title}` ? 'selected' : ''}" 
-    on:mouseenter="{() => current = `${mod.Title}`}" style="background-image: linear-gradient(1deg, rgb(0 0 0 / 0%), rgb(0 0 0 / 42%)), url('{mod.image}')">
+    on:mouseenter="{() => current = `${mod.Title}`}" style="background-image: linear-gradient(1deg, rgb(0 0 0 / 0%), rgb(0 0 0 / 90%)), url('{mod.image}')">
         <div class="object-center mx-[10%] mt-28 flex flex-col justify-center items-center">
             <h1 class="text-3xl font-semibold pt-12 text-white">{mod.Title}</h1>
             <p class="pt-8 text-white">{mod.Description}</p>
             <a class="mt-16 inline-flex py-2 px-7 text-l font-medium text-center rounded-full shadow-xl text-white bg-sky-500 border hover:text-sky-500 hover:font-bold hover:border border-sky-500 hover:bg-white hover:bg-opacity-60" href="/dashboard/${mod.Title}${$dets.newUrl}" data-sveltekit-prefetch type="button" 
             on:click="{() => show = `${mod.Title}`}" on:click="{() => clicked = `${mod.Title}`}" on:click="{press}"  on:click="{modPage}">
-            Get Started </a>
+            Let's go </a>
 
         </div>
     </div>
