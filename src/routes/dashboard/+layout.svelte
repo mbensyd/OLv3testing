58d53859946ddb6ff9b912e4440abee95656b2bf
emilycollins4 <emtcollins4@gmail.com>
2022-10-05 11:47:14 +1100

    update

--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -30,9 +30,10 @@
     </div>
   </nav>
   
- 
+<slot />
+
 <footer class="fixed bottom-0 left-0 z-20 p-4 w-full bg-white border-t bg-opacity-100 border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
-    <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://flowbite.com/" class="hover:underline">PGx™</a>
+    <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="#" class="hover:underline">PGx™</a>
     </span>
     <ul class="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
         <li>
@@ -46,7 +47,6 @@
   
   
 
-<slot />
 
 
 
