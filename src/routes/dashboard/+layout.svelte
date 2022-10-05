887940204a7511b04244ecdf32f8c9748cea8cea
emilycollins4 <emtcollins4@gmail.com>
2022-10-05 11:31:14 +1100

    new

--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -4,7 +4,7 @@
 
 <nav class="bg-white bg-opacity-20 px-2 sm:px-4 py-2.5 fixed w-full z-20 top-0 left-0">
     <div class="container flex flex-wrap justify-between items-center mx-auto">
-    <a href="#" class="flex items-center">
+    <a href={`/dashboard${$dets.newUrl}`} class="flex items-center">
         <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-6 sm:h-9" alt="Logo">
         <span class="self-center text-xl font-semibold whitespace-nowrap text-white">PGx</span>
     </a>
