ac66c3567ea2534ec71d4b6043cfe58afbf55b8c
badwolf11 <mbennett1190@gmail.com>
2022-10-05 16:57:59 +1100

    update
    Co-authored-by: Emily Collins <E.Collins3@westernsydney.edu.au>

--- src/routes/dashboard/[mod]/+page.js
+++ src/routes/dashboard/[mod]/+page.js
@@ -19,6 +19,5 @@ export async function load ({params, url}) {
         auth,
         endpoint,
         newUrl
-    }); 
-   
+    });
 };
--- src/routes/dashboard/[mod]/storeLink.js
+++ src/routes/dashboard/[mod]/storeLink.js
@@ -1,3 +1,33 @@
 import { writable } from 'svelte/store';
 
 export const actLink = writable('');
+
+//const regURL = encodeURIComponent($dets.reg);
+let regURL = 'cat';
+
+export const modData = writable(
+    { "modules": [
+        {
+            "Title": "Selecting your Fish",
+            "Description": "Stop the basic concepts of green supply chains and environmental management systems, including life cycle assessment.",
+            "image": "https://images.unsplash.com/photo-1500382017468-9049fed747ef?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80",
+            "toDoList": [{"text": "Task 1: Find some fish", "status": true, "link": "https://media.openlearning.com/" },
+                        {"text": "Task 2: Eat some fish", "status": false, "link": "https://cenariovr.com/app/#/view/i0q?zfile=live.zip&id=23354"},
+                        {"text": "Portfolio 1: Make a fishy", "status": false, "link": `https://media.openlearning.com/${regURL}`}]
+        },
+        {
+            "Title": "Selecting Optimal Genes",
+            "Description": "Learn the basic concepts of green supply chains and environmental management systems, including life cycle assessment.",
+            "image": "https://images.unsplash.com/photo-1502082553048-f009c37129b9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+            "toDoList": [{"text": "Task 1: Find some fish", "status": true},
+                        {"text": "Task 2: Eat some fish", "status": false},
+                        {"text": "Portfolio 1: Make a fishy", "status": false}]
+        },
+        {
+            "Title": "Selecting a Genetic Transformation Method",
+            "Description": "Learn the basic concepts of green supply chains and environmental management systems, including life cycle assessment.",
+            "image": "https://images.unsplash.com/photo-1580137189272-c9379f8864fd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+        }
+      ]
+        }
+)
