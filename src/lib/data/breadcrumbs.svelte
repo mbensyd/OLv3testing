ddcb745c8ede80902fd4434e14564d3c95610039
badwolf11 <mbennett1190@gmail.com>
2022-10-10 20:00:04 +1100

    breadcrumb fixes

--- src/lib/data/breadcrumbs.svelte
+++ src/lib/data/breadcrumbs.svelte
@@ -11,7 +11,7 @@ import { dets } from '../../routes/store.js';
               Mission
             </a>
           </li>
-          {#each Object.entries($BreadCrumbs) as [label, url] (label)}
+          {#each Object.entries($BreadCrumbs) as [label, url]}
           <li>
             <div class="flex items-center">
               <svg class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
