a0d4d25d077e78ead82ab0080e0624b0a11eecbc
badwolf11 <mbennett1190@gmail.com>
2022-10-10 17:18:28 +1100

    breadcrumbs working

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -8,11 +8,11 @@
     //breadcrumb
     import { BreadCrumbs } from '../store.js';
     $BreadCrumbs = {
-    Home: `/dashboard${dets.newUrl}`
+    Home: `#`
     };
 
     //cat
-    let name = $dets.userName;
+    let name = $dets.userName;s
 
     //images - need to be refactored so are dynamic. No idea how to link in sveltekit. 
    // import mod1 from '$lib/images/mod1.png';
